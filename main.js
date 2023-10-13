533022228aa25dbec13ea557e33e1166fffab56c
No-Mamun <94072559+No-Mamun@users.noreply.github.com>
2023-10-13 06:33:15 +0600

    Add files via upload
    parseFloat-parseInt-

---
+++ main.js
@@ -0,0 +1,14 @@
+
+/*
+var number_1 = "55";
+var number_2 = 99;
+
+number_1 = parseFloat(number_1);
+var mamun = number_1 + number_2;
+console.log(mamun);
+*/
+
+var number_1 = 20;
+var number_2 = 90;
+var total = number_1 + number_2;
+console.log(total);
